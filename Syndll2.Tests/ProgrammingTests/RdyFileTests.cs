9fc4ac78018c5f1fec067450da121c688ffb3c13
Matt Johnson <mj1856@hotmail.com>
2013-05-22 10:46:27 -0700

    Adding tests for rdy files

---
+++ Syndll2.Tests/ProgrammingTests/RdyFileTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Syndll2.Data;
+
+namespace Syndll2.Tests.ProgrammingTests
+{
+    [TestClass]
+    public class RdyFileTests
+    {
+        [TestMethod]
+        public void Can_Read_Normal_Rdy_File()
+        {
+            RdyFile rdy;
+            using (var stream = File.OpenRead(@"TestData\msg800.rdy"))
+            {
+                rdy = RdyFile.Read(stream);
+            }
+
+            Assert.IsNotNull(rdy);
+            Assert.AreEqual('V', rdy.Header.TableType);
+            Assert.AreEqual(800, rdy.Header.TableId);
+            Assert.AreEqual(64, rdy.Header.RecordCount);
+        }
+
+        [TestMethod]
+        public void Can_Read_Directory_Rdy_File()
+        {
+            RdyFile rdy;
+            using (var stream = File.OpenRead(@"TestData\dir001.rdy"))
+            {
+                rdy = RdyFile.Read(stream);
+            }
+
+            Assert.IsNotNull(rdy);
+            Assert.AreEqual('z', rdy.Header.TableType);
+            Assert.AreEqual(1, rdy.Header.TableId);
+            Assert.AreEqual(115, rdy.Header.TotalCharacters);
+        }
+    }
+}
