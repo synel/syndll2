309a7d7d5b35d4bbe4fb9771b9057bc57e54d3bf
Matt Johnson <mj1856@hotmail.com>
2013-06-11 20:24:19 -0700

    Version Bump

--- Syndll2/Properties/AssemblyInfo.cs
+++ Syndll2/Properties/AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("a7fe65a7-0b61-4ea1-81c0-adc5a037ea17")]
 
-[assembly: AssemblyVersion("0.3.1")]
-[assembly: AssemblyFileVersion("0.3.1.12")]
-[assembly: AssemblyInformationalVersion("0.3.1-beta")]
+[assembly: AssemblyVersion("0.3.2")]
+[assembly: AssemblyFileVersion("0.3.2.13")]
+[assembly: AssemblyInformationalVersion("0.3.2-beta")]
 
 [assembly: InternalsVisibleTo("Syndll2.Tests, PublicKey=" +
 "00240000048000009400000006020000002400005253413100040000010001000f0f0c410cd366" +
