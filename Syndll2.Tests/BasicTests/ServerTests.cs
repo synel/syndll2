371932081e92df942ecb864df90d57126c35361b
Matt Johnson <mj1856@hotmail.com>
2014-06-16 23:23:25 -0700

    Update unit test

--- Syndll2.Tests/BasicTests/ServerTests.cs
+++ Syndll2.Tests/BasicTests/ServerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Syndll2.Tests.BasicTests
@@ -9,28 +10,28 @@ namespace Syndll2.Tests.BasicTests
     {
         [TestMethod]
         [Ignore] // run this manually, after enabling polling mode and recording some data
-        public void Can_Receive_Inbound_Messages_From_Terminal()
+        public async Task Can_Receive_Inbound_Messages_From_Terminal()
         {
-            using (SynelServer.Listen(notification =>
-                {
-                    if (notification.Type == NotificationType.Data)
-                    {
-                        Console.WriteLine(notification.Data);
-                        notification.Acknowledege();
-                    }
-
-                    if (notification.Type == NotificationType.Query)
-                    {
-                        Console.WriteLine(notification.Data);
-                        notification.Reply(true, 0, "Success");
-                    }
-
-                }))
+            var server = new SynelServer();
+            server.MessageReceived += (sender, args) =>
             {
+                var notification = args.Notification;
+                if (notification.Type == NotificationType.Data)
+                {
+                    Console.WriteLine(notification.Data);
+                    notification.Acknowledege();
+                }
 
-                // run the server for awhile
-                Thread.Sleep(100000);
-            }
+                if (notification.Type == NotificationType.Query)
+                {
+                    Console.WriteLine(notification.Data);
+                    notification.Reply(true, 0, "Success");
+                }
+            };
+
+            var cts = new CancellationTokenSource();
+            server.ListenAsync(cts.Token).Wait(100000, cts.Token);
+
         }
     }
 }
